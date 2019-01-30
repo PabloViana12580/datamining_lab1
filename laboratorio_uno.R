8564ac1257e1628bd2c5d2fc662525806917a8ad
Pablo Viana <via16091@uvg.edu.gt>
2019-01-30 13:35:18 -0600

    Primer commit

---
+++ laboratorio_uno.R
@@ -0,0 +1,5 @@
+#Laboratorio 1
+#Pablo Viana - 16091
+#Sergio Marchena - 16387
+setwd("C:\\Users\\anali\\Desktop\\Pablito\\Septimo Semestre\\Mineria de datos\\datasets\\")
+peliculas<-read.csv("tmdb-movies.csv")
