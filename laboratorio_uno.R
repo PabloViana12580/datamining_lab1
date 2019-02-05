3e867bad0510b392eff4533a05025ea0cb7880f8
sergiomarchena16 <mar16387@uvg.edu.gt>
2019-02-04 22:54:26 -0600

    4.6-4.11
    falta el 4.5 y el 4.7

--- laboratorio_uno.R
+++ laboratorio_uno.R
@@ -4,7 +4,7 @@
 library(nortest)
 
 # Definimos el directorio de trabajo
-setwd("C:\\Users\\anali\\Desktop\\Pablito\\Septimo Semestre\\Mineria de datos\\datasets\\")
+setwd("~/Desktop/2019/MD/R/HT1/datamining_lab1")
 
 # cargamos la base de datos a una variable
 peliculas<-read.csv("tmdb-movies.csv")
@@ -50,20 +50,50 @@ qqline(peliculas$revenue_adj)
 #4 preguntas
 options(max.print = 50)
 
-#10 peliculas con mas presupuesto
+#4.1 10 peliculas con mas presupuesto
 orderData<-peliculas[c("original_title","budget","revenue","vote_count")]
 orderData[order(-orderData[,2]),]
 
-#10 peliculas con mejor ingreso
+#4.2 10 peliculas con mejor ingreso
 orderData[order(-orderData[,3]),]
 
-#Pelicula mas votada
+#4.3 Pelicula mas votada
 options(max.print = 5)
 orderData[order(-orderData[,4]),]
 
-#Pelicula con menos votos
+#4.4 Pelicula con menos votos
 orderData[order(orderData[,4]),]
 
-#Cuantas peliculas se hacen por a�o 
+#4.5 Cuantas peliculas se hacen por año 
 #pelixano = aggregate(peliculas[,16],list(peliculas$release_year), mean)
 #head(pelixano)
+
+#4.6 Género principal de las 20 películas más populares
+orden<-peliculas[c("original_title","vote_count","genres")]
+options(max.print = 20)
+orden[order(-orden[,2]),3]
+
+#4.7 género que predomina en el dataset - grafico
+
+#4.8 genero que obtuvieron mayores ganancias
+orden<-peliculas[c("original_title","revenue","genres")]
+options(max.print = 5)
+orden[order(-orden[,2]),3]
+
+#4.9 qué genero necesito mayor presupuesto
+View(orden)
+orden<-peliculas[c("original_title","budget","genres")]
+options(max.print = 5)
+orden[order(-orden[,2]),3]
+
+#4.10 20 mejores directores que hicieron películas altamente calificadas
+orden<-peliculas[c("original_title","vote_count","director")]
+options(max.print = 20)
+orden[order(-orden[,2]),3]
+
+#4.11 correlacion presupuestos - ingresos
+?cor
+hist(peliculas$budget)
+hist(peliculas$revenue)
+qqplot(peliculas$budget,peliculas$revenue)
+cor(peliculas$budget,peliculas$revenue)
